3f9f9786b98e5e5290695fb8bd1287d41dc2f5e7
Ivan <ivandaixivwork@gmail.com>
2020-03-03 14:41:04 -0800

    Adds icons to the navigation

--- js/navigation/BottomTabNav.js
+++ js/navigation/BottomTabNav.js
@@ -6,6 +6,8 @@ import MapScreen from '../screens/Map';
 import FavesScreen from '../screens/Faves';
 import AboutScreen from '../screens/About';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {sharedScreenOptions} from './config';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ScheduleStack = createStackNavigator();
 const MapStack = createStackNavigator();
@@ -14,27 +16,27 @@ const AboutStack = createStackNavigator();
 const BottomTabNav = createBottomTabNavigator();
 
 const ScheduleStackScreens = () => (
-  <ScheduleStack.Navigator>
+  <ScheduleStack.Navigator screenOptions={sharedScreenOptions}>
     <ScheduleStack.Screen name="Schedule" component={ScheduleScreen} />
     <ScheduleStack.Screen name="Session" component={SessionScreen} />
   </ScheduleStack.Navigator>
 );
 
 const MapStackScreens = () => (
-  <MapStack.Navigator>
+  <MapStack.Navigator screenOptions={sharedScreenOptions}>
     <MapStack.Screen name="Map" component={MapScreen} />
   </MapStack.Navigator>
 );
 
 const FavesStackScreens = () => (
-  <FavesStack.Navigator>
+  <FavesStack.Navigator screenOptions={sharedScreenOptions}>
     <FavesStack.Screen name="Faves" component={FavesScreen} />
     <FavesStack.Screen name="Session" component={SessionScreen} />
   </FavesStack.Navigator>
 );
 
 const AboutStackScreens = () => (
-  <AboutStack.Navigator>
+  <AboutStack.Navigator screenOptions={sharedScreenOptions}>
     <AboutStack.Screen name="About" component={AboutScreen} />
   </AboutStack.Navigator>
 );
@@ -44,9 +46,27 @@ const BottomTabNavScreens = props => (
     tabBarOptions={{
       activeTintColor: 'white',
       inactiveTintColor: '#999999',
-      labelStyle: {fontSize: 10},
+      labelStyle: {fontSize: 10, fontFamily: 'Montserrat'},
       style: {backgroundColor: 'black'},
-    }}>
+    }}
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Schedule') {
+          iconName = focused ? 'calendar-blank' : 'calendar-blank-outline';
+        } else if (route.name === 'Map') {
+          iconName = focused ? 'map' : 'map-outline';
+        } else if (route.name === 'Faves') {
+          iconName = focused ? 'heart' : 'heart-outline';
+        } else if (route.name === 'About') {
+          iconName = focused ? 'information' : 'information-outline';
+        }
+        return (
+          <MaterialCommunityIcons name={iconName} size={size} color={color} />
+        );
+      },
+    })}>
     <BottomTabNav.Screen name="Schedule" component={ScheduleStackScreens} />
     <BottomTabNav.Screen name="Map" component={MapStackScreens} />
     <BottomTabNav.Screen name="Faves" component={FavesStackScreens} />
