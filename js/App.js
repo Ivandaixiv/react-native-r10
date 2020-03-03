7e33048c9e0b338e7adf123aa168c794704c7e3e
Ivan <ivandaixivwork@gmail.com>
2020-03-02 16:33:05 -0800

    first commit

---
+++ js/App.js
@@ -0,0 +1,14 @@
+// js/App.js
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <View>
+          <Text>Welcome to React Native!</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
