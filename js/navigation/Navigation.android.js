aedd166513aebc2c539e6cfd061cfdaae6527117
Ivan <ivandaixivwork@gmail.com>
2020-03-09 13:47:15 -0700

    Adds icon to the drawer on android

--- js/navigation/Navigation.android.js
+++ js/navigation/Navigation.android.js
@@ -7,6 +7,7 @@ import FavesScreen from '../screens/Faves';
 import AboutScreen from '../screens/About';
 import {sharedScreenOptions, sharedBackOptions} from './config';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ScheduleStack = createStackNavigator();
 const MapStack = createStackNavigator();
@@ -51,7 +52,30 @@ const AboutStackScreens = () => (
 const Drawer = createDrawerNavigator();
 
 const DrawerNav = props => (
-  <Drawer.Navigator>
+  <Drawer.Navigator
+    drawerContentOptions={{
+      activeTintColor: '#9963ea',
+      labelStyle: {fontSize: 12, fontFamily: 'Montserrat'},
+      style: {backgroundColor: '#e6e6e6'},
+    }}
+    screenOptions={({route}) => ({
+      drawerIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Schedule') {
+          iconName = focused ? 'calendar-blank' : 'calendar-blank-outline';
+        } else if (route.name === 'Map') {
+          iconName = focused ? 'map' : 'map-outline';
+        } else if (route.name === 'Faves') {
+          iconName = focused ? 'heart' : 'heart-outline';
+        } else if (route.name === 'About') {
+          iconName = focused ? 'information' : 'information-outline';
+        }
+        return (
+          <MaterialCommunityIcons name={iconName} size={size} color={color} />
+        );
+      },
+    })}>
     <Drawer.Screen name="Schedule" component={ScheduleStackScreens} />
     <Drawer.Screen name="Map" component={MapStackScreens} />
     <Drawer.Screen name="Faves" component={FavesStackScreens} />
