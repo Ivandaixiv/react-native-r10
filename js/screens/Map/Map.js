a3a6ca62f9ea941f73f424137b9dd9c2c7b750b1
Ivan <ivandaixivwork@gmail.com>
2020-03-10 11:45:38 -0700

    Adds Apple's Mapview to Maps screen

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {Text} from 'react-native';
+import MapView from 'react-native-maps';
+import styles from './styles';
 
 const Map = () => {
-  return <Text>This is the Map Screen</Text>;
+  return (
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: 49.263388,
+        longitude: -123.138126,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      showsUserLocation={true}
+    />
+  );
 };
 export default Map;
