ef629cdbf43fc4a192e805efdcfdbba82188a9e3
Ivan <ivandaixivwork@gmail.com>
2020-03-10 14:06:42 -0700

    Adds maps screen to androids

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Platform, Image, View} from 'react-native';
 import MapView from 'react-native-maps';
+import {Marker} from 'react-native-maps';
 import styles from './styles';
 
 const Map = () => {
-  return (
+  return Platform.OS === 'ios' ? (
     <MapView
       style={styles.map}
       region={{
@@ -13,8 +14,15 @@ const Map = () => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
-      showsUserLocation={true}
-    />
+      showsUserLocation={true}>
+      <Marker coordinate={{latitude: 49.263388, longitude: -123.138126}}>
+        <Image source={require('../../assets/images/map_pin.png')} />
+      </Marker>
+    </MapView>
+  ) : (
+    <View style={styles.android}>
+      <Text>This feature is coming to Android soon!</Text>
+    </View>
   );
 };
 export default Map;
--- js/screens/Map/styles.js
+++ js/screens/Map/styles.js
@@ -3,5 +3,10 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+  android: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
 });
 export default styles;
